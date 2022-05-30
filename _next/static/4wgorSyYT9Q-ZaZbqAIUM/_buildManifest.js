d09fde7f93474ceb51a490e4d24969054cc3b056
Abhilkv <52311402+Abhilkv@users.noreply.github.com>
2022-05-30 12:36:10 +0000

    Deploying to gh-pages from @ Abhilkv/portfolio@c932b0edd6f4ec393d8863fece6412b7da521541 🚀

--- _next/static/4wgorSyYT9Q-ZaZbqAIUM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/595-677be91ce45779d0d717.js","static/css/2e6c3c207c04c1f88212.css","static/chunks/pages/index-ec404ee45ceb96f921ac.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
